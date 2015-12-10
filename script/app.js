0fc7835e6500d52a7f7f3c999a4ee6406e96d492
Johny Junior Duval <johny.duval@gmail.com>
2015-12-10 22:06:06 +0000

    Exercise 3: discovering forms

--- script/app.js
+++ script/app.js
@@ -1,1 +1,15 @@
 // Write your code here!
+$(document).ready(function(){
+    
+    $('#signup-form').on('submit', function(event) {
+    // write your code here
+    
+    if (($('input[name="password"]').val() === $('input[name="password2"]').val()) && $('input[name="password"]').val().length < 10 ){
+        event.preventDefault();
+        alert('All good!');
+    } else {
+        alert('You messed up!');
+    }
+});
+    
+})
