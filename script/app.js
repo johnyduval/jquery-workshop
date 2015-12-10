1c85a26eb9bdf70b8af59c96a070d73ea1a05de0
Johny Junior Duval <johny.duval@gmail.com>
2015-12-10 21:27:56 +0000

    Exercise 2: image gallery

--- script/app.js
+++ script/app.js
@@ -1,1 +1,12 @@
 // Write your code here!
+$(document).ready(function() {
+
+    $('img').on('click', function() {
+        $('body').append('<div class="overlay"><img src=' + $(this).attr("src") + '><p>' + $(this).attr("alt") + '</p></div>');
+
+        $('.overlay').on('click', function() {
+            $(this).remove();
+        });
+
+    });
+});
