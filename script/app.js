593dc8c40a44a7b0a8c11d9396974e9f317f238b
Johny Junior Duval <johny.duval@gmail.com>
2015-12-10 20:05:42 +0000

    Exercise 1: hiding/showing announcements

--- script/app.js
+++ script/app.js
@@ -1,1 +1,25 @@
 // Write your code here!
+$(document).ready(function() {
+
+    $('.announcement').prepend('<button data-text-swap="Show Announcement">X</button>');
+
+    $("button").on("click", function() {
+
+        $(this).siblings().toggle('slow');
+        
+        var el = $(this);
+        if (el.text() == el.data("text-swap")) {
+            el.text(el.data("text-original"));
+        }
+        else {
+            el.data("text-original", el.text());
+            el.text(el.data("text-swap"));
+        }
+    });
+
+
+    /*$('button').on('click', function(){
+        $(this).replaceWith("<button>Show Announcement</button>");
+        $('.announcement p').toggle('slow');
+    }); */
+});
