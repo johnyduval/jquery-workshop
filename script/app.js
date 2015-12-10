a731ed9efa46286207b454e7b9673f457fb55f4c
Johny Junior Duval <johny.duval@gmail.com>
2015-12-10 22:58:45 +0000

    incomplete ex. 4

--- script/app.js
+++ script/app.js
@@ -1,1 +1,10 @@
 // Write your code here!
+$(document).ready(function(){
+    
+    jQuery.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=montreal').then(
+    function(res) {
+        $('article > h2').after('<p>' + res.results[0].geometry.location + '</p>');
+    }
+);
+    
+})
